08d3884171407f5e0d1cb47f30f3f611c329bda0
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-27 13:45:48 +0300

    Update biggest_3.py

--- biggest_3.py
+++ biggest_3.py
@@ -1,18 +1,26 @@
-def biggest (a,b,c):
-    if a==b==c:
-        print("\nall numbers are equal")
-        quit()
+# פונקציה שמקבלת שלשה מספרים ומחזירה את הגדול מביניהם
+#גירסה ארוכה
+# def biggest (a,b,c):
+#     if a==b==c:
+#         print("\nall numbers are equal")
+#         quit()
 
-    if a>b and a>c:
-        return a
-    elif a>b and c>a:
-        return c
+#     if a>b and a>c:
+#         return a
+#     elif a>b and c>a:
+#         return c
         
-    if b>c and b>c:
-        return b    
-    elif b>a and b<c:
-        return c 
-    
-       
-        
+#     if b>c and b>c:
+#         return b    
+#     elif b>a and b<c:
+#         return c 
+            
+# print(biggest(1,12,33))
+
+
+#גירסה קצרה
+def biggest (a,b,c):
+    max_number = max(a,b,c)
+    return max_number
+
 print(biggest(1,12,33))
