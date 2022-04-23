53baca5a7d76a1dc9723e6d7f11bb784efffdcd4
MotiMatika <motimatika@gmail.com>
2022-04-23 21:20:59 +0300

    Update biggest_3.py

--- biggest_3.py
+++ biggest_3.py
@@ -26,18 +26,50 @@
 # print(biggest(1,12,33))
 
 
+#הגבוה מבין 6 מספרים
+#2 הפונקציות הראשונות מחזירות את המספר הגבוה מבין השלשה שהיא מקבלת
+#השלישית מקבלת את 2 הגבוהים ומדפיסה את הגבוה
+# def biggest1_3(a,b,c):
+#     max_1 = max(a,b,c)
+#     return(max_1)
+# A=biggest1_3(1,20,3)
+
+# def biggest4_6(a,b,c):
+#     max_2 = max(a,b,c)
+#     return(max_2)
+# B=biggest4_6(59,6,78)
+
+# def biggest1_6(A,B):
+#     maxx = max(A,B)
+#     return maxx
+# C=biggest1_6(A,B)
+# print(C)
+
+
+
+
+
+import random
+a=random.randint(1,100)
+b=random.randint(1,100)
+c=random.randint(1,100)
+print("\nthe first  3 numbers : ",a,b,c)
 def biggest1_3(a,b,c):
     max_1 = max(a,b,c)
     return(max_1)
-A=biggest1_3(1,20,3)
+A=biggest1_3(a,b,c)
 
-def biggest4_6(a,b,c):
-    max_2 = max(a,b,c)
+d=random.randint(1,100)
+e=random.randint(1,100)
+f=random.randint(1,100)
+print("the second 3 numbers : ",d,e,f)
+def biggest4_6(d,e,f):
+    max_2 = max(d,e,f)
     return(max_2)
-B=biggest4_6(59,6,78)
+B=biggest4_6(d,e,f)
 
 def biggest1_6(A,B):
     maxx = max(A,B)
     return maxx
 C=biggest1_6(A,B)
-print(C)
+print("the biggest  of the 6 numbers is : ", C)
